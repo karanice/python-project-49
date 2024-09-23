fe0a4c420814a3683f1c55ba5e12c45ddfe7d09f
karanice <to.anathema@gmail.com>
2024-09-23 20:27:44 +0300

    Update brain_games.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python 3
 
+from brain_games.cli import welcome_user
+
 def greeting():
     print('Welcome to the Brain Games!')
 
+
 def main():
     greeting()
+    print(f'Hello, {welcome_user()}!')
 
 if __name__ == '__main__':
     main()
