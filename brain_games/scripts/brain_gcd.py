a8478a3d3379ea92c2037ec9723c5028021bebaa
karanice <to.anathema@gmail.com>
2024-10-20 20:41:08 +0300

    add gcd and prime scripts

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from brain_games.engine.engine import procedure
+
+
+def main():
+    procedure('gcd')
+
+
+if __name__ == '__main__':
+    main()
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from brain_games.engine.engine import procedure
+
+
+def main():
+    procedure('prime')
+
+
+if __name__ == '__main__':
+    main()
