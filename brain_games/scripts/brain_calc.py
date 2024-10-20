c51b80d7557304ca970076ed0f7c3fe6dadc7aa0
karanice <to.anathema@gmail.com>
2024-10-20 20:44:02 +0300

    Update calc and even scripts

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,44 +1,10 @@
 #!/usr/bin/env python3
 
-from random import randint 
-from random import choice
-from brain_games.scripts.brain_games import greeting
-from brain_games.scripts.procedure import get_name
-from brain_games.scripts.procedure import get_answer
-from sys import exit
+from brain_games.engine.engine import procedure
 
 
 def main():
-    greeting()
-    user_name = get_name()
-    print('What is the result of the expression?')
-    opers = ['+', '-', '*']
- 
-
-    i = 0
-    while i != 3:
-        a = randint(0, 20)
-        b = randint(0, 20)
-        oper = choice(opers)
-
-        match oper:
-            case '+':
-                expr = a + b
-            case '-':
-                expr = a - b
-            case '*':
-                expr = a * b   
-        print(f'Question: {a}{oper}{b}')
-        user_answer = get_answer()
-
-        if user_answer == str(expr):
-            i += 1
-            print('Correct!')
-        else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{expr}'")
-            print(f'Let\'s try again, {user_name}!')
-            exit()
-    print(f'Congratulations, {user_name}!')
+    procedure('calc')
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,35 +1,11 @@
 #!/usr/bin/env python3
 
-from random import randint
-from brain_games.scripts.brain_games import greeting
-from brain_games.scripts.procedure import get_name
-from brain_games.scripts.procedure import get_answer
-from brain_games.games.even import print_even
-from brain_games.games.even import gen_and_check
+from brain_games.engine.engine import procedure
 
-from sys import exit
 
 def main():
-    greeting()
-    user_name = get_name()
-    print_even()
-    i = 0
-    while i != 3:
-        number, check = gen_and_check()
-        print(f'Question: {number}')
-        user_answer = get_answer()
+    procedure('even')
 
-        if user_answer == check:
-            i += 1
-            print('Correct!')
-        else:
-            i = 0
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{user_name}'")
-            print(f'Let\'s try again, {user_name}!')
-            exit()
-
-
-    print(f'Congratulations, {user_name}!')
 
 if __name__ == '__main__':
     main()
