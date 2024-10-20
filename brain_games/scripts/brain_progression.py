a074fcd6214a4bf8a43f6323e0e1ce955e2776ab
karanice <to.anathema@gmail.com>
2024-10-20 21:19:02 +0300

    Add progression script

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
+from brain_games.engine.engine import procedure
+
+
+def main():
+    procedure('progression')
+
+
+if __name__ == '__main__':
+    main()
