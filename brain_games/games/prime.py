b4b330983a1aba9fa5c3bcccc567f8a74b0517fa
karanice <to.anathema@gmail.com>
2024-10-21 12:23:42 +0300

    fix prime syntax

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,7 +7,7 @@ def print_prime():
 
 # number > 2
 def is_prime(number):
-    for i in range(2, number//2):
+    for i in range(2, number // 2):
         if number % i == 0:
             return 'no'
 
