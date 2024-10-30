fd9b4659ee4d800bb4f5197f3e7a621ba66ec119
karanice <to.anathema@gmail.com>
2024-10-30 21:37:44 +0300

    update cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,16 +1,4 @@
-#!/usr/bin/env python3
-
-import prompt
-
+from prompt import string
 
 def welcome_user():
-    name = prompt.string('May I have your name? ')
-    return name
-
-
-def main():
-    welcome_user()
-
-
-if __name__ == '__main__':
-    main()
+    return string('May I have your name? ')
