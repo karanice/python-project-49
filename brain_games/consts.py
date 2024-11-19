658e4c25e7ac63d053b10b0d0fe27d9f1070b11e
karanice <to.anathema@gmail.com>
2024-11-19 16:12:27 +0300

    make file with constants

--- brain_games/consts.py
+++ brain_games/consts.py
@@ -1,5 +1,8 @@
-q_calc = 'What is the result of the expression?'
-q_even = 'Answer "yes" if the number is even, otherwise answer "no".'
-q_gcd = 'Find the greatest common divisor of given numbers.'
-q_prime = 'Answer "yes" if given number is prime. Otherwise answer "no".'
-q_progression = 'What number is missing in the progression?'
+EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+CALC_INSTRUCTION = 'What is the result of the expression?'
+GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
+PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
+PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. ' \
+                    'Otherwise answer "no".'
+ROUND_NUMBERS = 3
+PROGRESSION_LENGTH = 10
