e0a49ed6ce4c3de92929e77ba7ecdc99d8a13c2b
karanice <to.anathema@gmail.com>
2024-11-24 18:59:19 +0300

    fix engine.py logic

--- brain_games/engine/engine.py
+++ brain_games/engine/engine.py
@@ -6,17 +6,17 @@ def run_game(get_question_and_answer, instruction):
     name = prompt.string('Welcome to the Brain Games!\nMay I have your name? ')
     print(f'Hello, {name}!\n{instruction}')
 
-    for round in range(0, ROUND_NUMBERS):
-        question, check = get_question_and_answer()
-        print('Question: ' + question)
+    for round in range(ROUND_NUMBERS):
+        question, answer = get_question_and_answer()
+        print(f'Question: {question}')
         user_answer = prompt.string('Your answer: ')
 
-        if user_answer == check:
+        if user_answer == str(answer):
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{check}\n'"
+                  f"Correct answer was '{answer}'\n"
                   f"Let\'s try again, {name}!")
-            return None
+            return
 
     print(f'Congratulations, {name}!')
