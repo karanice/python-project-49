2b7572e4d6b6cd354962611c7dfb3318e81761c8
karanice <to.anathema@gmail.com>
2024-11-30 14:23:56 +0300

    rename variables for better readability in gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -5,9 +5,9 @@ from brain_games.utils import get_random_number
 
 
 def get_nums_pair_and_gcd():
-    a, b = get_random_number(), get_random_number()
-    nums_pair = f'{a} {b}'
-    gcd = math.gcd(a, b)
+    first_num, second_num = get_random_number(), get_random_number()
+    nums_pair = f'{first_num} {second_num}'
+    gcd = math.gcd(first_num, second_num)
 
     return nums_pair, gcd
 
